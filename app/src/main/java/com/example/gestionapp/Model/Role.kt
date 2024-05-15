8952dc0a641b14d3d9085c45672f0ffdfc1af450
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-05-15 20:21:15 +0200

    Add Login Classes

---
+++ app/src/main/java/com/example/gestionapp/Model/Role.kt
@@ -0,0 +1,5 @@
+package com.example.gestionapp.Model
+
+enum class Role {
+    GENERAL, ADMIN
+}
---
+++ app/src/main/java/com/example/gestionapp/Model/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.gestionapp.Model
+
+class Usuario(var username: String, var password: String, var role: Role, var index: Int) {
+}
