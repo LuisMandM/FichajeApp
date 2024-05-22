c02b2f4c3677d1f63ef4d9d53334c04282bc5ce7
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-05-22 16:49:54 +0200

    Utilities n' repostory

--- app/src/main/java/com/example/gestionapp/BBDD/Repositorio.kt
+++ app/src/main/java/com/example/gestionapp/BBDD/Repositorio.kt
@@ -1,4 +1,31 @@
 package com.example.gestionapp.BBDD
 
-class Repositorio {
+import androidx.lifecycle.MutableLiveData
+import com.example.gestionapp.Model.Evento
+
+class Repositorio(val parser: BBDDParse) {
+    fun mostrarEventos(): MutableLiveData<List<Evento>> {
+        return parser.mostrarEventos()
+    }
+
+    fun insertarEvento(current: Evento) {
+        parser.insertarEvento(current)
+    }
+
+    fun borrarEvento(current: Evento) {
+        parser.borrarEvento(current)
+    }
+
+    fun modificarEvento(current: Evento) {
+        parser.modificarEvento(current)
+    }
+
+    fun idMaximo(): MutableLiveData<Int> {
+        return parser.buscarIdMaximo()
+    }
+
+    fun eventById(index: Int): MutableLiveData<Evento> {
+        return parser.eventById(index)
+    }
+
 }
--- app/src/main/java/com/example/gestionapp/Model/Utilitties.kt
+++ app/src/main/java/com/example/gestionapp/Model/Utilitties.kt
@@ -1,4 +1,21 @@
 package com.example.gestionapp.Model
 
 class Utilitties {
+
+    public fun enumValue(index:Int):EnumEvent{
+        return when(index){
+            0 -> EnumEvent.GENERAL
+            1 -> EnumEvent.GUARDIA
+            2 -> EnumEvent.REPORTE_HORARIO
+            else -> EnumEvent.GENERAL
+        }
+    }
+
+    fun indexEnum(tipo: EnumEvent): Int {
+        return when (tipo) {
+            EnumEvent.GENERAL -> 0
+            EnumEvent.GUARDIA -> 1
+            EnumEvent.REPORTE_HORARIO -> 2
+        }
+    }
 }
