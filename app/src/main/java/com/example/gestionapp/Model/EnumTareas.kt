4f4b60cbb39a4b5ac1e571aa5b9900e306dbcd40
LuisMandM <luisenrique.medina@ikasle.egibide.org>
2025-07-04 13:05:19 +0200

    Add Recyclerview to CheckBox

---
+++ app/src/main/java/com/example/gestionapp/Model/EnumTareas.kt
@@ -0,0 +1,6 @@
+package com.example.gestionapp.Model
+
+enum class EnumTareas {
+    Ingreso_Paciente, Alta_Paciente, Valoracion_Pte,Venopuncion,
+    Toma_Constantes_V, Glucometrias,Analiticas, Admin
+}
